315d87c681481a11ba45b34441ac93871bee8adc
chowdhury nayan <nayanchowdhury92@gmail.com>
2020-10-23 19:24:35 +0600

    Init changes

---
+++ Self.Host/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace Self.Host
+{
+    // http://www.codedigest.com/posts/1/what-is-owin-a-beginners-guide
+    // http://www.codedigest.com/posts/2/creating-our-first-owin-based-application
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebApp.Start("http://localhost:5001");
+            Console.WriteLine("Server Started...Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Self.Host/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Owin;
+
+namespace Self.Host
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("My First Owin Application");
+            });
+        }
+    }
+}
